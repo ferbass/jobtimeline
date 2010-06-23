db9c6656a5b691ed9ae24c7e7200c96607b7f28f
Fernando <ferbass@gmail.com>
2010-06-22 23:26:35 -0300

    config.gem => Warden 0.14.7

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
     config.gem "will_paginate", :version => "2.3.12"
     config.gem "devise", :version => "1.0.7"
-    config.gem "warden", :version => "0.10.4"
+    config.gem "warden", :version => "0.10.7"
     
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
