b3cbf6d8fce71d90e8dfca3aec4aa33a12e6373a
Fernando <ferbass@gmail.com>
2010-06-23 23:27:39 -0300

    Remove Prototype and add google api ajax 	modified:   app/views/layouts/application.html.erb 	deleted:    public/javascripts/controls.js 	deleted:    public/javascripts/dragdrop.js 	deleted:    public/javascripts/effects.js 	deleted:    public/javascripts/prototype.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.devise_for :users
-  
+  #map.root :controller => "application"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
